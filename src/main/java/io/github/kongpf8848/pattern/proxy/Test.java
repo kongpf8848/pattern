eaa54634826e7a3ba63d91ff695343408392aed2
kongpengfei <kongpf8848@gmail.com>
2022-09-18 23:16:50 +0800

    update

---
+++ src/main/java/io/github/kongpf8848/pattern/proxy/Test.java
@@ -0,0 +1,17 @@
+package io.github.kongpf8848.pattern.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+public class Test {
+    public static void main(String[] args) {
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
+        RealSubject realSubject = new RealSubject();
+        InvocationHandler handler = new DynamicProxy(realSubject);
+        Subject subject = (Subject) Proxy.newProxyInstance(Subject.class.getClassLoader(),
+                new Class<?>[] { Subject.class }, handler);
+        System.out.println(subject.getClass().getName());
+
+        subject.hello("jack");
+    }
+}
