23dd60e14527f8ebf1a88d964d4b5eb2cc9475a1
kongpf <kongpf@zenmen.com>
2022-09-20 18:19:36 +0800

    update

--- src/main/java/io/github/kongpf8848/pattern/proxy/Test.java
+++ src/main/java/io/github/kongpf8848/pattern/proxy/Test.java
@@ -1,5 +1,13 @@
 package io.github.kongpf8848.pattern.proxy;
 
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import javax.lang.model.element.Modifier;
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
@@ -13,5 +21,34 @@ public class Test {
         System.out.println(subject.getClass().getName());
 
         subject.hello("jack");
+
+        aa();
+    }
+
+    public static void aa(){
+        try {
+            TypeSpec.Builder typeSpecBuilder = TypeSpec.classBuilder("TimeProxy").addSuperinterface(Subject.class);
+
+            FieldSpec fieldSpec = FieldSpec.builder(Subject.class, "subject", Modifier.PRIVATE).build();
+            typeSpecBuilder.addField(fieldSpec);
+
+            //添加构造函数
+            MethodSpec constructSpec= MethodSpec.constructorBuilder()
+                    .addModifiers(Modifier.PUBLIC)
+                    .addParameter(Subject.class,"subject")
+                    .addStatement("this.subject=subject")
+                    .build();
+
+            typeSpecBuilder.addMethod(constructSpec);
+
+            //
+
+            JavaFile javaFile = JavaFile.builder("io.github.kongpf8848.pattern.proxy", typeSpecBuilder.build()).build();
+            javaFile.writeTo(new File("/Users/kongpf/Desktop/"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
